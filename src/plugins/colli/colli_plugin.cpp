7c8d419beef340852d3daaf83ffee1d61984ec20
Bahram Maleki-Fard <maleki-fard@kbsg.rwth-aachen.de>
2013-07-13 16:25:30 +0200

    colli: remove redundant old commented code from colli_plugin.cpp

--- src/plugins/colli/colli_plugin.cpp
+++ src/plugins/colli/colli_plugin.cpp
@@ -25,8 +25,7 @@
 #include "colli_thread.h"
 
 #ifdef HAVE_VISUAL_DEBUGGING
-#include "visualization_thread.h"
-//#include "navigation_thread.h"
+ #include "visualization_thread.h"
 #endif
 
 using namespace fawkes;
@@ -36,19 +35,14 @@ class ColliPlugin : public fawkes::Plugin
  public:
   ColliPlugin(Configuration *config) : Plugin(config)
   {
-    // thread_list.push_back(new ColliThread());
     ColliThread *collithr = new ColliThread();
     thread_list.push_back(collithr);
-    /* #ifdef HAVE_VISUAL_DEBUGGING
-    ColliNavigationThread *navthr = new ColliNavigationThread();
-    collithr->set_navigation_thread(navthr);
-    thread_list.push_back(navthr);
-    #endif*/
-    #ifdef HAVE_VISUAL_DEBUGGING
+
+#ifdef HAVE_VISUAL_DEBUGGING
     ColliVisualizationThread *visthr = new ColliVisualizationThread();
     collithr->set_visualization_thread(visthr);
     thread_list.push_back(visthr);
-    #endif
+#endif
   }
 };
 
